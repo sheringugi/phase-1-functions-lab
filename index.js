880f1dd8f8de4ac5aa03815e83e1a97c925fd9ae
sheringugi <sheri.ngugi17@gmail.com>
2023-03-14 08:59:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+function distanceFromHqInBlocks(pickup){
+    if(pickup > 42)
+    {
+        return (pickup - 42)
+    }
+    else if (pickup < 42){
+        return (42 - pickup)
+    }
+}
+function distanceFromHqInFeet (pickup) {
+    return distanceFromHqInBlocks(pickup)*264
+}
+function distanceTravelledInFeet(pick, drop){
+    if (drop>pick){
+      return(drop-pick)*264
+    }
+      else {
+        return (pick-drop)*264
+      }
+} 
+function calculatesFarePrice(pick,drop){
+let distance= distanceTravelledInFeet(pick, drop)
+    if(distance<=400){
+    return 0
+}
+else if (distance >=400 && distance<=2000){
+    return (distance-400)*0.02
+}
+else if (distance>=2000 && distance<=2500){
+    return 25
+}
+else if (distance > 2500){
+    return "cannot travel that far"
+}
+
+}
 // Code your solution in this file!
